a115be01ea3d02f4a810939f9fac918c23536a30
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-06-30 15:23:40 +0800

    feat: cleaned user policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -18,7 +18,7 @@ class UserPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, User $model): bool
+    public function view(User $user): bool
     {
         return $user->hasPermission('manage_users');
     }
@@ -34,7 +34,7 @@ class UserPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, User $model): bool
+    public function update(User $user): bool
     {
         return $user->hasPermission('manage_users');
     }
@@ -42,7 +42,7 @@ class UserPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, User $model): bool
+    public function delete(User $user): bool
     {
         return $user->hasPermission('manage_users');
     }
