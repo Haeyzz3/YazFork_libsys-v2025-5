af09b08e0e039bd8cfa317189092a70c6b6a8575
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-06-30 13:53:41 +0800

    feat: live admins ma men

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    public function index()
+    {
+        $admins = User::whereHas('role', function($query) {
+            $query->whereIn('role_name', ['admin', 'super_admin']);
+        })->get();
 
+        return view('manage-admins', compact('admins'));
+    }
 }
