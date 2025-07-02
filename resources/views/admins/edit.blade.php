d9f590dc88628399d05b7db153d80dc586ef9d4c
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-07-02 15:08:34 +0800

    feat: manage admins, edit form functional and received

--- resources/views/admins/edit.blade.php
+++ resources/views/admins/edit.blade.php
@@ -1,9 +1,123 @@
 <x-layouts.app :title="__('Edit Admin')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+        <!--
+          This example requires some changes to your config:
 
-        admins update form here
-{{--        @foreach($admins as $admin)--}}
-{{--            <p>{{ $admin->username }}</p>--}}
-{{--        @endforeach--}}
+          ```
+          // tailwind.config.js
+          module.exports = {
+            // ...
+            plugins: [
+              // ...
+              require('@tailwindcss/forms'),
+            ],
+          }
+          ```
+        -->
+        <form action="{{ route('admins.update', $admin) }}" method="POST">
+            @csrf
+            @method('PUT')
+
+            <div class="space-y-12">
+
+                <div class="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
+                    <div>
+                        <h2 class="text-base font-semibold leading-7 text-gray-900">Edit Admin</h2>
+                        <p class="mt-1 text-sm leading-6 text-gray-600">Update administrator account information and credentials.</p>
+                    </div>
+
+                    <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
+                        <div class="sm:col-span-3">
+                            <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
+                            <div class="mt-2">
+                                <input
+                                    value="{{ $admin->first_name }}"
+                                    id="first-name"
+                                    name="first-name"
+                                    type="text"
+                                    autocomplete="given-name"
+                                    placeholder="Enter your first name"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    value="{{ old('first-name') }}"
+                                    required
+                                    @error('first-name') ring-red-500 focus:ring-red-500 @enderror
+                                >
+                                @error('first-name')
+                                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="sm:col-span-3">
+                            <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Last name</label>
+                            <div class="mt-2">
+                                <input
+                                    value="{{ $admin->last_name }}"
+                                    id="last-name"
+                                    name="last-name"
+                                    type="text"
+                                    autocomplete="family-name"
+                                    placeholder="Enter your last name"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    value="{{ old('last-name') }}"
+                                    required
+                                    @error('last-name') ring-red-500 focus:ring-red-500 @enderror
+                                >
+                                @error('last-name')
+                                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="sm:col-span-4">
+                            <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
+                            <div class="mt-2">
+                                <input
+                                    value="{{ $admin->username }}"
+                                    id="username"
+                                    name="username"
+                                    type="text"
+                                    autocomplete="username"
+                                    placeholder="Enter your username"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    value="{{ old('username') }}"
+                                    required
+                                    @error('username') ring-red-500 focus:ring-red-500 @enderror
+                                >
+                                @error('username')
+                                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="sm:col-span-4">
+                            <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                            <div class="mt-2">
+                                <input
+                                    value="{{ $admin->email }}"
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    autocomplete="email"
+                                    placeholder="Enter your email address"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    value="{{ old('email') }}"
+                                    required
+                                    @error('email') ring-red-500 focus:ring-red-500 @enderror
+                                >
+                                @error('email')
+                                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mt-6 flex items-center justify-end gap-x-6">
+                <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+                <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+            </div>
+        </form>
     </div>
 </x-layouts.app>
