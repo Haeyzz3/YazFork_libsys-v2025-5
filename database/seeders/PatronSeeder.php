727a3a8b80e11d278ccc50d64d81f73245a2719b
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-07-03 10:18:12 +0800

    feat: manage patrons, added patron role to users

--- database/seeders/PatronSeeder.php
+++ database/seeders/PatronSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,14 @@ class PatronSeeder extends Seeder
      */
     public function run(): void
     {
+        $patron = Role::firstOrCreate(['role_name' => 'patron']);
+
         User::create([
             'first_name' => 'Patron',
             'last_name' => 'Student',
             'username' => '@patron',
             'email' => 'patron@email.com',
+            'role_id' =>  $patron->id,
             'password' => bcrypt('patron123'),
         ])->patronDetails()
             ->create([
