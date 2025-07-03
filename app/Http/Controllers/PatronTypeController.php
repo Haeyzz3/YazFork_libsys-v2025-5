253064c02dcd7365be29529d192b0dc4f939d0bd
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-07-03 11:56:32 +0800

    feat: bulk model -mcr

---
+++ app/Http/Controllers/PatronTypeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PatronType;
+use Illuminate\Http\Request;
+
+class PatronTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PatronType $patronType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PatronType $patronType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PatronType $patronType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PatronType $patronType)
+    {
+        //
+    }
+}
