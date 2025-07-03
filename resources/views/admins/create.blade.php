c44e905732d1e263d8b8deae2dd485e604776d30
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-07-03 15:13:44 +0800

    feat: new add admin for new db

--- resources/views/admins/create.blade.php
+++ resources/views/admins/create.blade.php
@@ -25,6 +25,7 @@
                     </div>
 
                     <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
+
                         <div class="sm:col-span-3">
                             <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
                             <div class="mt-2">
@@ -65,7 +66,45 @@
                             </div>
                         </div>
 
-                        <div class="sm:col-span-4">
+                        <div class="sm:col-span-3">
+                            <label for="middle-name" class="block text-sm font-medium leading-6 text-gray-900">Middle Name</label>
+                            <div class="mt-2">
+                                <input
+                                    id="middle-name"
+                                    name="middle-name"
+                                    type="text"
+                                    autocomplete="additional-name"
+                                    placeholder="Enter your middle name"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    value="{{ old('middle-name') }}"
+                                    @error('middle-name') ring-red-500 focus:ring-red-500 @enderror
+                                >
+                                @error('middle-name')
+                                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="sm:col-span-3">
+                            <label for="birth-date" class="block text-sm font-medium leading-6 text-gray-900">Birth Date</label>
+                            <div class="mt-2">
+                                <input
+                                    id="birth-date"
+                                    name="birth-date"
+                                    type="date"
+                                    autocomplete="bday"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    value="{{ old('birth-date') }}"
+                                    required
+                                    @error('birth-date') ring-red-500 focus:ring-red-500 @enderror
+                                >
+                                @error('birth-date')
+                                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="sm:col-span-3">
                             <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
                             <div class="mt-2">
                                 <input
@@ -82,6 +121,16 @@
                                 @error('username')
                                 <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
                                 @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-span-3">
+                            <label for="photo" class="block text-sm font-medium leading-6 text-gray-900">Photo</label>
+                            <div class="mt-2 flex items-center gap-x-3">
+                                <svg class="h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                    <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
+                                </svg>
+                                <button type="button" class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Change</button>
                             </div>
                         </div>
 
