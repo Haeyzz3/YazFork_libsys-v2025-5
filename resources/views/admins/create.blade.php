bec0abbb7b7a7d27e13947bba94e1ec3fd86d7c8
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-07-04 17:53:18 +0800

    feat: contact number on create view

--- resources/views/admins/create.blade.php
+++ resources/views/admins/create.blade.php
@@ -134,7 +134,7 @@
                             </div>
                         </div>
 
-                        <div class="sm:col-span-4">
+                        <div class="sm:col-span-3">
                             <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                             <div class="mt-2">
                                 <input
@@ -149,6 +149,26 @@
                                     @error('email') ring-red-500 focus:ring-red-500 @enderror
                                 >
                                 @error('email')
+                                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="sm:col-span-3">
+                            <label for="contact-number" class="block text-sm font-medium leading-6 text-gray-900">Contact Number</label>
+                            <div class="mt-2">
+                                <input
+                                    id="contact-number"
+                                    name="contact-number"
+                                    type="tel"
+                                    autocomplete="tel"
+                                    placeholder="Enter your contact number"
+                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    value="{{ old('contact-number') }}"
+                                    required
+                                    @error('contact-number') ring-red-500 focus:ring-red-500 @enderror
+                                >
+                                @error('contact-number')
                                 <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
                                 @enderror
                             </div>
@@ -199,7 +219,9 @@
 
             <div class="mt-6 flex items-center justify-end gap-x-6">
                 <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-                <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white
+                shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
+                focus-visible:outline-indigo-600">Add admin</button>
             </div>
         </form>
     </div>
