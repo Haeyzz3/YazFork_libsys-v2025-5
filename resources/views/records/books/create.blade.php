6a5221744243a8f98350680401b2020408ad98b2
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-07-08 00:02:39 +0800

    feat: create book, physical description

--- resources/views/records/books/create.blade.php
+++ resources/views/records/books/create.blade.php
@@ -223,6 +223,142 @@
                     </div>
                 </div>
 
+                <div class="space-y-8 mt-8">
+                    <div>
+                        <h2 class="text-base font-semibold leading-7 text-gray-900">Physical Description</h2>
+                    </div>
+                    <div class="grid max-w-6xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-3">
+
+                        <div class="sm:col-span-1">
+                            <label for="edition" class="block text-sm font-medium leading-6 text-gray-900">Edition</label>
+                            <div class="mt-2">
+                                <input
+                                    id="edition"
+                                    name="edition"
+                                    type="text"
+                                    placeholder="Enter edition"
+                                    class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm
+                           ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
+                           focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    value="{{ old('edition') }}"
+                                >
+                                @error('edition')
+                                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="sm:col-span-1">
+                            <label for="cover-type" class="block text-sm font-medium leading-6 text-gray-900">Cover Type</label>
+                            <div class="mt-2">
+                                <select
+                                    id="cover-type"
+                                    name="cover-type"
+                                    class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm
+                           ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600
+                           sm:text-sm sm:leading-6"
+                                >
+                                    <option value="" disabled selected>Select cover type</option>
+                                    <option value="Hardcover" {{ old('cover-type') == 'Hardcover' ? 'selected' : '' }}>Hardcover</option>
+                                    <option value="Paperback" {{ old('cover-type') == 'Paperback' ? 'selected' : '' }}>Paperback</option>
+                                    <option value="Spiral-bound" {{ old('cover-type') == 'Spiral-bound' ? 'selected' : '' }}>Spiral-bound</option>
+                                    <option value="Other" {{ old('cover-type') == 'Other' ? 'selected' : '' }}>Other</option>
+                                </select>
+                                @error('cover-type')
+                                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="sm:col-span-1">
+                            <label for="book-cover-image" class="block text-sm font-medium leading-6 text-gray-900">Book Cover Image</label>
+                            <div class="mt-2">
+                                <input
+                                    id="book-cover-image"
+                                    name="book-cover-image"
+                                    type="file"
+                                    accept=".jpg,.jpeg,.png,.pdf"
+                                    class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm
+                           ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600
+                           sm:text-sm sm:leading-6"
+                                >
+                                @error('book-cover-image')
+                                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="space-y-8 mt-8">
+                    <div>
+                        <h2 class="text-base font-semibold leading-7 text-gray-900">Physical Description</h2>
+                    </div>
+                    <div class="grid max-w-6xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-3">
+
+                        <div class="sm:col-span-1">
+                            <label for="edition" class="block text-sm font-medium leading-6 text-gray-900">Edition</label>
+                            <div class="mt-2">
+                                <input
+                                    id="edition"
+                                    name="edition"
+                                    type="text"
+                                    placeholder="Enter edition"
+                                    class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm
+                           ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
+                           focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    value="{{ old('edition') }}"
+                                >
+                                @error('edition')
+                                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="sm:col-span-1">
+                            <label for="cover-type" class="block text-sm font-medium leading-6 text-gray-900">Cover Type</label>
+                            <div class="mt-2">
+                                <select
+                                    id="cover-type"
+                                    name="cover-type"
+                                    class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm
+                           ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600
+                           sm:text-sm sm:leading-6"
+                                >
+                                    <option value="" disabled selected>Select cover type</option>
+                                    <option value="Hardcover" {{ old('cover-type') == 'Hardcover' ? 'selected' : '' }}>Hardcover</option>
+                                    <option value="Paperback" {{ old('cover-type') == 'Paperback' ? 'selected' : '' }}>Paperback</option>
+                                    <option value="Spiral-bound" {{ old('cover-type') == 'Spiral-bound' ? 'selected' : '' }}>Spiral-bound</option>
+                                    <option value="Other" {{ old('cover-type') == 'Other' ? 'selected' : '' }}>Other</option>
+                                </select>
+                                @error('cover-type')
+                                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="sm:col-span-1">
+                            <label for="book-cover-image" class="block text-sm font-medium leading-6 text-gray-900">Book Cover Image</label>
+                            <div class="mt-2">
+                                <input
+                                    id="book-cover-image"
+                                    name="book-cover-image"
+                                    type="file"
+                                    accept=".jpg,.jpeg,.png,.pdf"
+                                    class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm
+                           ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600
+                           sm:text-sm sm:leading-6"
+                                >
+                                @error('book-cover-image')
+                                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
                 <div class="mt-6 flex items-center justify-end gap-x-6">
                     <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
                     <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add Book</button>
