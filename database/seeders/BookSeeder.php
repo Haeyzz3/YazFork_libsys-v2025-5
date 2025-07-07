6e0b3b8a60850639781d98fc9cbff2450047f7f3
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-07-07 21:45:06 +0800

    Revert "feat: book factory done"
    This reverts commit 35c2d7865ea4b9a37033f1b52974e270a953af8c.

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -14,7 +14,7 @@ class BookSeeder extends Seeder
     public function run(): void
     {
         Record::factory()
-            ->has(Book::factory(), 'book')
+            ->has(Book::factory(), 'books')
             ->count(20)->create();
     }
 }
