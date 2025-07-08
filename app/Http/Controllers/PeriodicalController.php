c2435038461b9eeefeafb86500efea40cca4529a
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-07-08 23:08:12 +0800

    feat: periodical index -mcr

---
+++ app/Http/Controllers/PeriodicalController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Record;
+use Illuminate\Http\Request;
+
+class PeriodicalController extends Controller
+{
+    public function index()
+    {
+        $records = Record::with('digitalResource')
+            ->whereHas('digitalResource')
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+
+        return view('records.periodicals.index', [
+            'records' => $records,
+        ]);
+    }
+}
