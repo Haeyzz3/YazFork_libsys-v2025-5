acbc0e8a144a2b43e7587007499eaa92effc0d66
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-07-08 11:55:15 +0800

    feat: layouts in records index

---
+++ resources/views/components/layouts/records.blade.php
@@ -0,0 +1,60 @@
+<x-layouts.app :title="__('Manage Patrons')">
+    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+
+        <div class="px-4 sm:px-6 lg:px-8">
+            <div class="sm:flex sm:items-center">
+                <div class="sm:flex-auto">
+                    <h1 class="text-base font-semibold leading-6 text-gray-900">Manage Records</h1>
+                </div>
+            </div>
+
+            <div>
+                <div class="sm:hidden">
+                    <label for="tabs" class="sr-only">Select a tab</label>
+                    <!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
+                    <select id="tabs" name="tabs" class="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500">
+                        <option>My Account</option>
+                        <option>Company</option>
+                        <option selected>Team Members</option>
+                        <option>Billing</option>
+                    </select>
+                </div>
+                <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none flex justify-end gap-2">
+                    <a href="{{ route('books.create') }}">
+                        <button type="button" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold
+                         text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2
+                         focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                            Add book
+                        </button>
+                    </a>
+                </div>
+                <div class="hidden sm:block">
+                    <div class="border-b border-gray-200">
+                        <nav class="-mb-px flex space-x-8" aria-label="Tabs">
+                            {{-- Add Book Tab --}}
+                            <a href="{{ route('books.index') }}"
+                               class="group inline-flex items-center border-b-2 {{ request()->routeIs('books.index') ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700' }} px-1 py-4 text-sm font-medium">
+                                <svg class="-ml-0.5 mr-2 h-5 w-5 {{ request()->routeIs('books.create') ? 'text-indigo-500' : 'text-gray-400 group-hover:text-gray-500' }}" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                    <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
+                                </svg>
+                                <span>Books</span>
+                            </a>
+
+                            <a href="{{ route('e-collections.index') }}"
+                               class="group inline-flex items-center border-b-2 {{ request()->routeIs('e-collections.index') ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700' }} px-1 py-4 text-sm font-medium">
+                                <svg class="-ml-0.5 mr-2 h-5 w-5 {{ request()->routeIs('books.create') ? 'text-indigo-500' : 'text-gray-400 group-hover:text-gray-500' }}" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                    <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
+                                </svg>
+                                <span>E-collections</span>
+                            </a>
+
+                        </nav>
+                    </div>
+                </div>
+            </div>
+
+            {{ $slot }}
+        </div>
+
+    </div>
+</x-layouts.app>
--- resources/views/records/books/index.blade.php
+++ resources/views/records/books/index.blade.php
@@ -1,99 +1,44 @@
-<x-layouts.app :title="__('Manage Patrons')">
-    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+<x-layouts.records>
+    <div class="mt-8 flow-root">
+        <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="inline-block min-w-full py-2 align-middle">
+                <table class="min-w-full divide-y divide-gray-300">
+                    <thead>
+                    <tr>
+                        <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">Accession no.</th>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Title</th>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">DDC classification</th>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Acquisition Status</th>
+                        <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6 lg:pr-8">
+                            <span class="sr-only">Edit</span>
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody class="divide-y divide-gray-200 bg-white">
 
-        <div class="px-4 sm:px-6 lg:px-8">
-            <div class="sm:flex sm:items-center">
-                <div class="sm:flex-auto">
-                    <h1 class="text-base font-semibold leading-6 text-gray-900">Manage Records</h1>
-                </div>
+                    @foreach($records as $record)
+                        <tr>
+                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">{{ $record->accession_number }}</td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $record->title }}</td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $record->ddc_classification }}</td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $record->acquisition_status }}</td>
+                            <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 lg:pr-8">
+                                <a href="{{ route('books.show', $record) }}" class="text-indigo-600 hover:text-indigo-900">View all details</a>
+                            </td>
+                        </tr>
+                    @endforeach
+
+                    <!-- More people... -->
+                    </tbody>
+                </table>
             </div>
-
-            <div>
-                <div class="sm:hidden">
-                    <label for="tabs" class="sr-only">Select a tab</label>
-                    <!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
-                    <select id="tabs" name="tabs" class="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500">
-                        <option>My Account</option>
-                        <option>Company</option>
-                        <option selected>Team Members</option>
-                        <option>Billing</option>
-                    </select>
-                </div>
-                <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none flex justify-end gap-2">
-                    <a href="{{ route('books.create') }}">
-                        <button type="button" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold
-                         text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2
-                         focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                            Add book
-                        </button>
-                    </a>
-                </div>
-                <div class="hidden sm:block">
-                    <div class="border-b border-gray-200">
-                        <nav class="-mb-px flex space-x-8" aria-label="Tabs">
-                            {{-- Add Book Tab --}}
-                            <a href="{{ route('books.index') }}"
-                               class="group inline-flex items-center border-b-2 {{ request()->routeIs('books.index') ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700' }} px-1 py-4 text-sm font-medium">
-                                <svg class="-ml-0.5 mr-2 h-5 w-5 {{ request()->routeIs('books.create') ? 'text-indigo-500' : 'text-gray-400 group-hover:text-gray-500' }}" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
-                                </svg>
-                                <span>Books</span>
-                            </a>
-
-                            <a href="{{ route('e-collections.index') }}"
-                               class="group inline-flex items-center border-b-2 {{ request()->routeIs('e-collections.index') ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700' }} px-1 py-4 text-sm font-medium">
-                                <svg class="-ml-0.5 mr-2 h-5 w-5 {{ request()->routeIs('books.create') ? 'text-indigo-500' : 'text-gray-400 group-hover:text-gray-500' }}" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
-                                </svg>
-                                <span>E-collections</span>
-                            </a>
-
-                        </nav>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="mt-8 flow-root">
-                <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div class="inline-block min-w-full py-2 align-middle">
-                        <table class="min-w-full divide-y divide-gray-300">
-                            <thead>
-                            <tr>
-                                <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">Accession no.</th>
-                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Title</th>
-                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">DDC classification</th>
-                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Acquisition Status</th>
-                                <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6 lg:pr-8">
-                                    <span class="sr-only">Edit</span>
-                                </th>
-                            </tr>
-                            </thead>
-                            <tbody class="divide-y divide-gray-200 bg-white">
-
-                            @foreach($records as $record)
-                                <tr>
-                                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">{{ $record->accession_number }}</td>
-                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $record->title }}</td>
-                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $record->ddc_classification }}</td>
-                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $record->acquisition_status }}</td>
-                                    <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 lg:pr-8">
-                                        <a href="{{ route('books.show', $record) }}" class="text-indigo-600 hover:text-indigo-900">View all details</a>
-                                    </td>
-                                </tr>
-                            @endforeach
-
-                            <!-- More people... -->
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Render pagination links -->
-        <div class="py-8">
-            {{ $records->links() }}
         </div>
     </div>
-</x-layouts.app>
+
+    <!-- Render pagination links -->
+    <div class="py-8">
+        {{ $records->links() }}
+    </div>
+
+</x-layouts.records>
+
