a834e981ba055cb78c3bd8e2f4fa12183ffc189f
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-07-08 08:56:51 +0800

    feat: alpine on action, success and error

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,4 +1,25 @@
 <x-layouts.app.sidebar :title="$title ?? null">
+    @if(session('success'))
+        <div
+            x-data="{ show: true}"
+            x-show="show"
+            x-init="setTimeout( () => show = false, 5000 )"
+            class="fixed top-4 right-4 bg-green-700 text-white p-4 rounded shadow-lg">
+            <span>{{ session('success') }}</span>
+            <button @click="show = false" class="ml-4 text-white hover:text-gray-200">×</button>
+        </div>
+    @endif
+
+    @if(session('error'))
+        <div
+            x-data="{ show: true}"
+            x-show="show"
+            x-init="setTimeout( () => show = false, 5000 )"
+            class="fixed top-4 right-4 bg-red-700 text-white p-4 rounded shadow-lg">
+            <span>{{ session('error') }}</span>
+            <button @click="show = false" class="ml-4 text-white hover:text-gray-200">×</button>
+        </div>
+    @endif
     <flux:main>
         {{ $slot }}
     </flux:main>
