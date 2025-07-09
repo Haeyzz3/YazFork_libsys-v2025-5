a4c95823d5aaaad76f2ca96ce8adec1b3f4fa011
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-07-09 15:16:32 +0800

    feat: done creation of thesis

--- resources/views/records/thesis/create.blade.php
+++ resources/views/records/thesis/create.blade.php
@@ -401,7 +401,6 @@
                             class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm
                    ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
                    focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            required
                         >
                         @error('abstract-document')
                         <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
@@ -422,7 +421,6 @@
                             class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm
                    ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
                    focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            required
                         >
                         @error('full-text')
                         <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
