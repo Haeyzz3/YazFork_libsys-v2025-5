d3171d4670b637926f58c1f7f00328d423edf7e3
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-07-11 04:22:16 +0800

    fix:  the classic Alpine.js "flash of content" issue!

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{ open: @entangle('isModalOpen') }" x-show="open" class="fixed inset-0 flex items-center justify-center z-50">
+<div x-data="{ open: @entangle('isModalOpen') }" x-show="open" x-cloak class="fixed inset-0 flex items-center justify-center z-50">
     <div class="bg-white z-30 h-[95vh] w-[80vw] relative overflow-y-auto dark:bg-gray-800 p-6 rounded-lg shadow-lg">
         <button wire:click="closeModal" class="mt-4 px-4 py-2 absolute right-4 top-4 bg-gray-200 dark:bg-gray-600 rounded">Close</button>
         {{ $slot }}
