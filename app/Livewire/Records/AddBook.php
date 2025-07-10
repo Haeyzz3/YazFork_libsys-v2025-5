8eb6ecec5304498c8f1ee9886865a2d00e4b0c30
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-07-10 13:26:58 +0800

    feat: add book livewire start

---
+++ app/Livewire/Records/AddBook.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Records;
+
+use Livewire\Component;
+
+class AddBook extends Component
+{
+    public function render()
+    {
+        return view('livewire.records.add-book');
+    }
+}
