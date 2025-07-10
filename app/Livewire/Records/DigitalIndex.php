0a4ddd82f6a09d5e95b9c8b04ac12e5e45c4047f
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-07-10 08:56:45 +0800

    feat: digital index

---
+++ app/Livewire/Records/DigitalIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Records;
+
+use Livewire\Component;
+
+class DigitalIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.records.digital-index');
+    }
+}
