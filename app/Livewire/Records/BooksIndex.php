21984526e41e59ddfd96f1f6e5d1d8231aeca2da
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-07-23 11:32:17 +0800

    feat: import all records, bigger file size limit

--- app/Livewire/Records/BooksIndex.php
+++ app/Livewire/Records/BooksIndex.php
@@ -44,7 +44,7 @@ class BooksIndex extends Component
     public function rules()
     {
         return [
-            'import_csv' => 'required|file|mimes:csv,txt|max:2048',
+            'import_csv' => 'required|file|mimes:csv,txt|max:20480',
         ];
     }
 
