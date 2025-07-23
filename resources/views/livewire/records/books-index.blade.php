198887535e552967d487d9b50d813e37710dc51c
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-07-23 13:47:13 +0800

    feat: polish, enough title width

--- resources/views/livewire/records/books-index.blade.php
+++ resources/views/livewire/records/books-index.blade.php
@@ -51,8 +51,7 @@
                     @forelse($records as $record)
                         <tr>
                             <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">{{ $record->accession_number }}</td>
-                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-600">{{ $record->title }}</td>
-                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-600">{{ $record->book->ddcClassification->name ?? $record->lc_classification }}</td>
+                            <td class="px-3 py-4 text-sm text-gray-600 max-w-md truncate">{{ $record->title }}</td>                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-600">{{ $record->book->ddcClassification->name ?? $record->lc_classification }}</td>
                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-600">{{ $record->book->authors ?? 'Not specified' }}</td>
                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-600">{{ $record->book->publication_year ?? 'Not specified' }}</td>
                             <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 lg:pr-8">
