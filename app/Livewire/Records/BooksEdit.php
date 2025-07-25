341e8bf81bf4d1c45542a6d84b3ed98ec924d1d1
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-07-25 10:36:37 +0800

    feat: book edit, livewire

---
+++ app/Livewire/Records/BooksEdit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Records;
+
+use Livewire\Component;
+
+class BooksEdit extends Component
+{
+    public function render()
+    {
+        return view('livewire.records.books-edit');
+    }
+}
--- resources/views/records/books/show.blade.php
+++ resources/views/records/books/show.blade.php
@@ -259,9 +259,9 @@
 
         <div class="mt-4 flex justify-end px-4">
             <a href="{{ route('books.edit', $record) }}">
-                <button type="button" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold
-                        text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2
-                        focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Edit Book</button>
+                <button type="button" class="rounded-md bg-accent px-3 py-2 text-sm font-semibold
+                        text-white shadow-sm hover:bg-accent-content focus-visible:outline focus-visible:outline-2
+                        focus-visible:outline-offset-2 focus-visible:outline-accent">Edit Book</button>
             </a>
         </div>
     </div>
